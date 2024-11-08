5d42bfd7c21fd90486ac229184d7c51ff171018a
Suraj <suraj.ukumar.p@gmail.com>
2024-11-08 23:39:45 +0530

    feat: the project complete as plan.

--- src/app/api/course/createChapters/route.ts
+++ src/app/api/course/createChapters/route.ts
@@ -4,9 +4,20 @@ import { ZodError } from "zod";
 import { strict_output } from "@/lib/gpt";
 import { getUnsplashImage } from "@/lib/unsplash";
 import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/auth";
+import { checkSubscription } from "@/lib/subscription";
 
 export async function POST(req: Request) {
   try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return NextResponse.json("Unauthorized", { status: 401 });
+    }
+    const isPro = await checkSubscription();
+    if (session.user.credits <= 0 && !isPro) {
+      return NextResponse.json("no credits", { status: 402 });
+    }
+
     const body = await req.json();
     const { title, units } = createChaptersSchema.parse(body);
 
@@ -83,6 +94,17 @@ export async function POST(req: Request) {
         }),
       });
     }
+
+    await prisma.user.update({
+      where: {
+        id: session.user.id,
+      },
+      data: {
+        credits: {
+          decrement: 1,
+        },
+      },
+    });
 
     return NextResponse.json({ course_id: course.id });
   } catch (error) {
